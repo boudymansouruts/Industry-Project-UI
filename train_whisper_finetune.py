d09359f3a09ec418a96a5f40d9a46ae969ed9378
boudymansouruts <boudy.mansour@student.uts.edu.au>
2025-10-06 00:33:20 +1100

    Disable gradient checkpointing

--- train_whisper_finetune.py
+++ train_whisper_finetune.py
@@ -150,8 +150,7 @@ def main() -> None:
         load_best_model_at_end=True,
         metric_for_best_model="loss",
         greater_is_better=False,
-        gradient_checkpointing=True,
-        gradient_checkpointing_kwargs={"use_reentrant": True},
+        gradient_checkpointing=False,
     )
 
     trainer = Trainer(
